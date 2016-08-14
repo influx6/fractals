6be8dfbd75f13bd7995706d28c4c197b3fde7b58
Apple <apple@Apples-MacBook-Air.local>
2016-08-14 13:19:32 +0100

    fixing issues

--- netd/con.go
+++ netd/con.go
@@ -6,7 +6,7 @@ import "net"
 // to manage the request-response cycle of a provided net.Conn.
 type Provider interface {
 	Close(context interface{}) error
-	SendMessage(context interface{},msg []byte) error
+	SendMessage(context interface{}, msg []byte) error
 	CloseNotify() chan struct{}
 }
 
@@ -15,28 +15,27 @@ type Provider interface {
 // expanding more details from a central controller or within a decentral
 // controller.
 type Broadcast interface {
-	SendToClients(context interface{}, msg[]byte) error
-	SendToClusters(context interface{},msg []byte) error
+	SendToClients(context interface{}, msg []byte) error
+	SendToClusters(context interface{}, msg []byte) error
 }
 
-// StatProvider provides a interfce which allows access to operations on
-// stats items.
-type StatProvider interface {
-  IncrementInMsg()
-  IncrementOutMsg()
-  IncrementRequest()
-  IncrementReads(size int)
-  IncrementWrites(size int)
+// Connection defines a struct which stores the incoming request for a
+// connection.
+type Connection struct {
+	net.Conn
+	Config Config
+	Stat   Stat
 }
 
-// Handler defines a function handler which
-type Handler func(context interface{}, net.Conn, Config) (Provider, error)
+// Handler defines a function handler which returns a new Provider from a
+// Connection.
+type Handler func(context interface{}, c *Connection) (Provider, error)
 
 // Conn defines an interface which manages the connection creation and accept
 // lifecycle and using the provided ConnHandler produces connections for
 // both clusters and and clients.
 type Conn interface {
 	Broadcast
-	ServeClient(context interface{}, Handler) error
-	ServeCluster(context interface{}, Handler) error
+	ServeClient(context interface{}, h Handler) error
+	ServeCluster(context interface{}, h Handler) error
 }
--- netd/stat.go
+++ netd/stat.go
@@ -1,1 +1,47 @@
 package netd
+
+// StatProvider provides a interfce which allows access to operations on
+// stats items.
+type StatProvider interface {
+  IncrementInMsg()
+  IncrementOutMsg()
+  IncrementRequest()
+  IncrementReads(size int)
+  IncrementWrites(size int)
+}
+
+// Stat defines a struct for storing statistics data recieved from a provided 
+// Conn.
+type Stat struct {
+	InMsg        int64
+	OutMsg       int64
+	OutBytes     int64
+	InBytes      int64
+	Requests     int64
+	TotalClients int64
+}
+
+// IncrementWrites increments the InByte counter.
+func (stat Stat) IncrementWrites(size int) {
+	atomic.AddInt64(&stat.OutBytes, int64(size))
+}
+
+// IncrementOutMsg increments the OutMsg counter.
+func (stat Stat) IncrementRead(size int) {
+	atomic.AddInt64(&stat.InBytes, int64(size))
+}
+
+// IncrementInMsg increments the InMsg counter.
+func (stat Stat) IncrementInMsg() {
+	atomic.AddInt64(&stat.InMsg, 1)
+}
+
+// IncrementOutMsg increments the OutMsg counter.
+func (stat Stat) IncrementOutMsg() {
+	atomic.AddInt64(&stat.OutMsg, 1)
+}
+
+// IncrementRequest increments the Requests counter.
+func (stat Stat) IncrementRequest() {
+	atomic.AddInt64(&stat.Requests, 1)
+}
